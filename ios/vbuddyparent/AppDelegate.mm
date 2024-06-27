fcfc344729de65cef4b338cfb4526a5f8907ca15
a.azarbashi <a.azarbashi@trustimm.ca>
2024-06-27 14:19:56 +0330

    add pack

--- ios/vbuddyparent/AppDelegate.mm
+++ ios/vbuddyparent/AppDelegate.mm
@@ -27,5 +27,15 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+- (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
+                          moduleName:(NSString *)moduleName
+                           initProps:(NSDictionary *)initProps {
+  UIView *rootView = [super createRootViewWithBridge:bridge
+                                          moduleName:moduleName
+                                           initProps:initProps];
 
+  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView];
+
+  return rootView;
+}
 @end
